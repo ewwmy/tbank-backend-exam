8ea7a978275ba2f50cd100083c66cbf259c99cf5
Andrew Akimov <akimov.it.dev@gmail.com>
2024-08-05 03:35:08 +0300

    Багфикс: Поправил URL в Swagger

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const options = {
     },
     servers: [
       {
-        url: `http://localhost:${PORT}`,
+        url: `http://localhost:${PORT}/api`,
         description: `Локальный сервер, использующий порт ${PORT}`,
       },
     ],
